b78ee788e489cb27a5e2c654b76184f26b0cd4d1
valner <valner47@gmail.com>
2013-12-07 22:51:01 +0400

    regatta fix

--- script/regatta/start_2ex.sh
+++ script/regatta/start_2ex.sh
@@ -2,7 +2,7 @@
 
 # Compile and run jobs
 # Use: start <source file>
-BINARY= `echo $1 | sed  -r 's/(.*)\..*/\1/g'`
+BINARY=`echo $1 | sed  -r 's/(.*)\..*/\1/g'`
 ./compile_2ex.sh $1 $BINARY
 ./run_2ex.sh $BINARY
 
