734e3b35f9dc7b9fbde98d06834061d05ec029c8
valner <valner47@gmail.com>
2013-12-07 22:30:06 +0400

    fixes 4 bluegene

--- script/bluegene/login.sh
+++ script/bluegene/login.sh
@@ -6,8 +6,14 @@ source ../../svt.conf
 readonly BLUEGENE=10.6.7.50
 readonly HOST=$RG_BG_LOGIN@$BLUEGENE
 readonly KEY=$ROOT/keys/$RG_BG_LOGIN
-scp  -i $KEY $ROOT/script/regatta/* $ROOT/src/*  $HOST:/home/$RG_BG_LOGIN  
+if [ $USER != "root" ];
+then
+    echo "Please execute this script with root rights"
+    exit 1
+fi
+$TERMINAL -e "cd $ROOT/vpn/; openvpn --config cmc.ovpn" >/dev/null 2>&1 &
+sleep 10
+scp  -i $KEY $ROOT/script/regatta/* $ROOT/src/*  $HOST:/gpfs/data/vmk-edu-2013/$RG_BG_LOGIN  
 
-$TERMINAL -e "openvpn --config $ROOT/keys/cmc.ovpn"
 $TERMINAL -e "ssh $HOST -i $KEY" >/dev/null 2>&1 &
 
